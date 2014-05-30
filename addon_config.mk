02d89b2d4c2a96d870676ce862c7c9e7e672d319
bakercp <me@christopherbaker.net>
2014-05-30 01:41:01 +0000

    Added addon_config.mk

---
+++ addon_config.mk
@@ -0,0 +1,10 @@
+meta:
+	ADDON_NAME = ofxGeo
+	ADDON_DESCRIPTION = An addon for geographic tasks.
+	ADDON_AUTHOR = bakercp
+	ADDON_TAGS = "geo" "mapping" "gps"
+	ADDON_URL = http://github.com/bakercp/ofxGeo
+common:
+	# dependencies with other addons, a list of them separated by spaces 
+	# or use += in several lines
+	ADDON_DEPENDENCIES = ofxSpatialHash 
