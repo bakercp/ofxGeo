b199215bb60f6544d60fcf46a86484448fe2e508
Christopher Baker <me@christopherbaker.net>
2017-01-26 09:31:37 -0600

    Working on CI.

--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -1,8 +1,52 @@
 #!/usr/bin/env bash
 set -e
 
-echo `pwd`
+# Get the current OF_ROOT
+OF_ROOT="${OF_ROOT:-~/openFrameworks}"
+OF_ADDONS=$OF_ROOT/addons
+# Addon path
+# These paths assume that this file is located in
+#   ADDON_NAME/ci/install.sh
 
-python3 scripts/ci/common/install_required_addons.py
+ADDON_PATH=$( cd $(dirname $0) ; cd ../../; pwd -P )
+ADDON_NAME=${ADDON_PATH##*/}
 
-ls -la ~/openFrameworks/addons
+GH_USERNAME='bakercp'
+GH_BRANCH='master'
+GH_DEPTH=1
+
+REQUIRED_ADDONS=()
+
+# Extract ADDON_DEPENDENCIES from addon_config.mk file.
+if [ -f $ADDON_PATH/addon_config.mk ]; then
+  while read line; do
+    #line="$(echo -e "${line}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+    if [[ $line == ADDON_DEPENDENCIES* ]] ;
+    then
+      line=${line#*=}
+      IFS=' ' read -ra ADDR <<< "$line"
+      for i in "${ADDR[@]}"; do
+          REQUIRED_ADDONS+=($i)
+      done
+    fi
+  done < $ADDON_PATH/addon_config.mk
+fi
+
+# Gather addons from all examples.
+for addons_make in $ADDON_PATH/example*/addons.make; do
+  while read addon; do
+    if [ $addon != $ADDON_NAME ] ;
+    then
+      REQUIRED_ADDONS+=($addon)
+    fi
+  done < $addons_make
+done
+
+# We aren't de-duplicating array, to keep it pure bash.
+for addon in "${REQUIRED_ADDONS[@]}"
+do
+  addon_path=$OF_ADDONS/$addon
+  if [ ! -d "$addon_path" ]; then
+    git clone --depth=$GH_DEPTH https://github.com/$GH_USERNAME/$addon.git $addon_path
+  fi
+done
